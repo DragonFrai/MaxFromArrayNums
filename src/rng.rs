af46008e7e57bb26ed8e1e6dbf234b1bcb2f3fbe
Vladislav Podporkin <vavus44375@gmail.com>
2021-03-09 14:15:58 +0300

    initial commit

---
+++ src/rng.rs
@@ -0,0 +1,31 @@
+
+
+// В стандартной библиотекке нет генератора случайных чисел. Пишем просто xor-shift
+pub struct Rng {
+    a: u64,
+    b: u64,
+}
+
+impl Rng {
+    pub fn new(seed: u64) -> Self {
+        Self { a: seed, b: !seed }
+    }
+
+    pub fn gen_i32_in(&mut self, min: i32, max: i32) -> i32 {
+        let d = max - min + 1;
+        let r = (self.gen_u64() % (d as u64)) as i32;
+        r + min
+    }
+
+    pub fn gen_u64(&mut self) -> u64 {
+        let mut t = self.a;
+        let s = self.b;
+        self.a = s;
+        t ^= t << 23;
+        t ^= t << 17;
+        t ^= s ^ (s >> 26);
+        self.b = t;
+        t.wrapping_add(s)
+    }
+
+}
